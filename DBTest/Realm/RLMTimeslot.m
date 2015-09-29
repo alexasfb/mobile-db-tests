f4f5030fb9fe8aca7df6efa9ca4941b8ba26830a
JP Simard <jp@jpsim.com>
2015-09-29 11:39:11 -0700

    Address issues leading to mis-profiling Realm
    1. Run DBTest scheme in Release mode.
    2. Perform NSDateFormatter conversion before benchmarking.
    3. Use +[RLMObject createInRealm:withValue:] rather than constructing an object
       in memory and then copying it over to the Realm.
    
    These minor modifications yield the following results:
    ​
    
    * CORE DATA: 4241 ops/second
    * REALM: 14290 ops/second
    * COUCHBASE SQLITE: 161 ops/second
    * COUCHBASE FOREST DB: 157 ops/second
    
    * CORE DATA: 26 ops/second
    * REALM: 22299 ops/second
    * COUCHBASE SQLITE: 35 ops/second
    * COUCHBASE FOREST DB: 664 ops/second

--- DBTest/Realm/RLMTimeslot.m
+++ DBTest/Realm/RLMTimeslot.m
@@ -12,26 +12,25 @@
 
 @implementation RLMTimeslot
 
-- (void) setFields:(NSDictionary*)fields {
-    self.activity = [fields objectForKey:FIELD_TIMESLOT_ACTIVITY];
-    self.idTimeslot = [fields objectForKey:FIELD_TIMESLOT_ID];
-    self.idActivity = [fields objectForKey:FIELD_TIMESLOT_ACTIVITY_ID];
-    
-    NSDateFormatter* formatter = [NSDateFormatter dateFormatterForFormat:FORMAT_TIMESLOT_DATE];
++ (NSDictionary *)mapFields:(NSDictionary *)fields {
+    NSDateFormatter *formatter = [NSDateFormatter dateFormatterForFormat:FORMAT_TIMESLOT_DATE];
     //Dates send from server are using the US format. We need to set the locale of this DateFormater to be sure the date will be parse properly.
     //On iPhone OS, the user can override the default AM/PM versus 24-hour time setting (via Settings > General > Date & Time > 24-Hour Time), which causes NSDateFormatter to rewrite the format string
     [formatter setLocale:[NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"]];
-    self.start = [formatter dateFromString:[fields objectForKey:FIELD_TIMESLOT_START]];
-    self.end = [formatter dateFromString:[fields objectForKey:FIELD_TIMESLOT_END]];
-    
-    self.idFake = [fields objectForKey:FIELD_TIMESLOT_FAKE_ID];
-    
-    self.managerNotes = [fields objectForKey:FIELD_TIMESLOT_MANAGER_NOTES];
-    self.guideNotes = [fields objectForKey:FIELD_TIMESLOT_GUIDE_NOTES];
-    self.meetingLocation = [fields objectForKey:FIELD_TIMESLOT_MEETING_LOCATION];
-    self.time_series_type = [fields objectForKey:FIELD_TIMESLOT_TIME_SERIES_TYPE];
-    self.availabilityStatus = [fields objectForKey:FIELD_TIMESLOT_AVAILABILITY_STATUS];
-}
 
+    return @{
+             @"activity": fields[FIELD_TIMESLOT_ACTIVITY],
+             @"idTimeslot": fields[FIELD_TIMESLOT_ID],
+             @"idActivity": fields[FIELD_TIMESLOT_ACTIVITY_ID],
+             @"start": [formatter dateFromString:fields[FIELD_TIMESLOT_START]],
+             @"end": [formatter dateFromString:fields[FIELD_TIMESLOT_END]],
+             @"idFake": fields[FIELD_TIMESLOT_FAKE_ID],
+             @"managerNotes": fields[FIELD_TIMESLOT_MANAGER_NOTES],
+             @"guideNotes": fields[FIELD_TIMESLOT_GUIDE_NOTES],
+             @"meetingLocation": fields[FIELD_TIMESLOT_MEETING_LOCATION],
+             @"time_series_type": fields[FIELD_TIMESLOT_TIME_SERIES_TYPE],
+             @"availabilityStatus": fields[FIELD_TIMESLOT_AVAILABILITY_STATUS]
+     };
+}
 
 @end
